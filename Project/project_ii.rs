1be6fcf6e9f1c37f76e211332a36740ea6e8970a
Mimi <moyo.iyanda@pau.edu.ng>
2025-10-11 14:10:47 +0100

    (My First Project)

---
+++ Project/project_ii.rs
@@ -0,0 +1,17 @@
+fn main() {
+	let t:f64 = 450000.0;
+	let m:f64 = 1500000.0;
+	let h:f64 = 750000.0;
+	let d:f64 = 2850000.0;
+	let a:f64 = 250000.0;
+
+	// sum
+	let s = t + m + h + d + a;
+	let q = 2.0 + 1.0 + 3.0 + 3.0 + 1.0;
+	let a = s/ q;
+	
+	println!("average is {} and the sum is {}", a, s);
+
+}
+
+
