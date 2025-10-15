fa247991cb71b2189a7b275621d2a8c2d167ef6b
Mimi <moyo.iyanda@pau.edu.ng>
2025-10-15 20:06:32 +0100

    (My fifth practice)

---
+++ week-3/practice-5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'B';
+    let surname = "Bolaji";
+    let first_name = "Micheal";
+    let middle_name = "Gboyega";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, first_name, middle_name);
+    
+}
