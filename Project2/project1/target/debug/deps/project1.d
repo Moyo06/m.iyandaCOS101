83f93133f0121bc6ce8ea338aad70e0f4a66a406
Mimi <moyo.iyanda@pau.edu.ng>
2025-10-24 19:39:12 +0100

    (My first project)

---
+++ Project2/project1/target/debug/deps/project1.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\m.iyandaCOS101\Project2\project1\target\debug\deps\project1.d: src\main.rs
+
+C:\Users\New\Documents\m.iyandaCOS101\Project2\project1\target\debug\deps\project1.exe: src\main.rs
+
+src\main.rs:
---
+++ Project2/project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\m.iyandaCOS101\Project2\project1\target\debug\project1.exe: C:\Users\New\Documents\m.iyandaCOS101\Project2\project1\src\main.rs
