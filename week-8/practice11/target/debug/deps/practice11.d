fadfff03a62e239bb2ce758daec35884452b2aa5
Mimi <moyo.iyanda@pau.edu.ng>
2025-11-19 18:04:33 +0100

    (Rust slice)

---
+++ week-8/practice11/target/debug/deps/practice11.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\m.iyandaCOS101\week-8\practice11\target\debug\deps\practice11.d: src\main.rs
+
+C:\Users\New\Documents\m.iyandaCOS101\week-8\practice11\target\debug\deps\practice11.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice11/target/debug/practice11.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\m.iyandaCOS101\week-8\practice11\target\debug\practice11.exe: C:\Users\New\Documents\m.iyandaCOS101\week-8\practice11\src\main.rs
