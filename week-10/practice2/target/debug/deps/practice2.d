debdb2c0503f4ac4d68cf774b72c9502e4db27ff
Mimi <moyo.iyanda@pau.edu.ng>
2025-12-03 13:13:53 +0100

    Passing value to a function

---
+++ week-10/practice2/target/debug/deps/practice2.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\m.iyandaCOS101\week-10\practice2\target\debug\deps\practice2.d: src\main.rs
+
+C:\Users\New\Documents\m.iyandaCOS101\week-10\practice2\target\debug\deps\practice2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice2/target/debug/practice2.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\m.iyandaCOS101\week-10\practice2\target\debug\practice2.exe: C:\Users\New\Documents\m.iyandaCOS101\week-10\practice2\src\main.rs
